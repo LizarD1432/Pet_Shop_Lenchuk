f6a124e53146906d5e6adc78cae42d1972389a6b
Lizardus <gitmail@mail.com>
2024-11-18 07:46:35 +0300

    pet_shop

---
+++ SQL/manual_table_insert.sql
@@ -0,0 +1,105 @@
+use petShop_db
+
+/*create table Category
+(
+	id int primary key identity(1,1),
+	categoryName nvarchar(100)
+)
+
+create table Supplier
+(
+	id int primary key identity(1,1),
+	supplierName nvarchar(100)
+)
+
+create table Producer
+(
+	id int primary key identity(1,1),
+	producerName nvarchar(100)
+)
+
+create table ProductType
+(
+	id int primary key identity(1,1),
+	productTypeName nvarchar(100)
+)
+
+create table QuantityType
+(
+	id int primary key identity(1,1),
+	quantityTypeName nvarchar(50)
+)*/
+
+/*create table Products
+(
+	id int primary key identity(1,1),
+	productTypeId int,
+	quantityTypeId int,
+	cost int,
+	maxDiscount int,
+	producerId int,
+	supplierId int,
+	categoryId int,
+	currentDiscount int,
+	amount int,
+	description nvarchar(200),
+	image nvarchar(100)
+)*/
+
+/*create table Status
+(
+	id int primary key identity(1,1),
+	statusName nvarchar(100)
+)*/
+
+/*create table Orders
+(
+	id int primary key identity(1,1),
+	idStatus int,
+	composition nvarchar(100),
+	dateOfOrder date,
+	dateOfDelivery date,
+	point int,
+	lname nvarchar(100),
+	fname nvarchar(100),
+	patro nvarchar(100),
+	code int
+)*/
+
+/*create table Street
+(
+	id int primary key identity(1,1),
+	streetName nvarchar(100)
+)
+
+create table City
+(
+	id int primary key identity(1,1),
+	CityName nvarchar(100)
+)*/
+
+/*create table Point
+(
+	id int primary key identity(1,1),
+	indexNumber nvarchar(100),
+	idCity int,
+	idStreet int,
+	number int
+)*/
+
+/*create table Roles
+(
+	id int primary key identity(1,1),
+	roleName nvarchar(100)
+)
+
+create table Users
+(
+	id int primary key identity(1,1),
+	idRoles int,
+	lname nvarchar(100),
+	fnaem nvarchar(100),
+	patro nvarchar(100),
+	login nvarchar(100),
+	password nvarchar(100)
+)*/
